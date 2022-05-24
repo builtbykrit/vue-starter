910063c4d5bcf55029234df5e326366c38d3ead6
Jen Dooley <jen@krit.com>
2022-05-24 15:52:01 -0600

    Created VOtpField

---
+++ src/components/_library/forms/VOtp/VOtpField.vue
@@ -0,0 +1,165 @@
+<template>
+  <VInputSkin :input-id="internalId" :label="label" :messages="errorMessages">
+    <div ref="inputsContainer" class="flex items-center gap-2">
+      <VOtpSingleInput
+        v-for="(character, index) in characters"
+        ref="inputs"
+        :key="`OTP-${index}`"
+        :model-value="displayValue[index]"
+        :focused="state.focused"
+        :readonly="readonly"
+        :disabled="disabled"
+        :has-error="hasError"
+        @focus="onFocus(index)"
+        @blur="onBlur"
+        @paste="onPaste(index, $event)"
+        @keydown="onKeyDown(index, $event)"
+        @input="onInput(index, $event)"
+      />
+    </div>
+  </VInputSkin>
+</template>
+
+<script setup>
+import { computed, reactive, ref, watch } from "vue"
+import { useValidation } from "@/composables/useValidation"
+import { v4 as uuid } from "uuid"
+
+const props = defineProps({
+  modelValue: {
+    type: [Array, String],
+    default: () => Array(6).fill(""),
+    validator: (value) => value.length < 6 || value.length === 6,
+  },
+  label: { type: String, default: "      " },
+  characters: { type: Number, default: 6 },
+  readonly: Boolean,
+  disabled: Boolean,
+  validation: {
+    type: Object,
+    default: () => ({
+      $dirty: false,
+      $error: false,
+      $errors: [],
+    }),
+  },
+})
+const emit = defineEmits(["update:modelValue", "completed"])
+const { hasError, errorMessages } = useValidation(props.validation)
+
+const inputs = ref(null)
+const state = reactive({
+  focused: false,
+  activeInput: 0,
+})
+
+const internalId = computed(() => (props.id ? props.id : uuid()))
+const displayValue = computed(() =>
+  typeof props.modelValue !== "string"
+    ? props.modelValue
+    : [
+        ...props.modelValue,
+        ...Array(props.characters).fill("").splice(props.characters),
+      ]
+)
+const isComplete = computed(() => props.modelValue.length === props.characters)
+watch(isComplete, (value) => {
+  if (value) emit("completed")
+})
+
+const onFocus = (index) => {
+  state.focused = true
+  state.activeInput = index
+}
+
+const onBlur = () => {
+  state.focused = false
+  state.activeInput = -1
+}
+
+const onInput = (index, char) => {
+  const newValue = Object.assign([], props.modelValue)
+  newValue[index] = char
+
+  if (newValue.join("") !== props.modelValue) {
+    emit("update:modelValue", newValue.join(""))
+    focusNextInput()
+  }
+}
+
+const onPaste = (index, event) => {
+  event.preventDefault()
+  const pastedData = event.clipboardData
+    .getData("text/plain")
+    .slice(0, props.characters - state.activeInput)
+    .split("")
+
+  // Paste data from focused input onwards
+  const currentCharsInOtp = displayValue.value.slice(0, state.activeInput)
+  const combinedWithPastedData = currentCharsInOtp
+    .concat(pastedData)
+    .slice(0, props.characters)
+
+  const newValue = Object.assign([], displayValue.value)
+  combinedWithPastedData.forEach(function (value, i) {
+    newValue[i] = value
+  })
+
+  if (combinedWithPastedData.length === props.characters) {
+    state.focused = false
+    state.activeInput = -1
+  } else {
+    focusInput(combinedWithPastedData.slice(0, props.characters).length)
+  }
+  emit("update:modelValue", newValue.join(""))
+}
+
+const focusInput = (inputIndex) => {
+  state.activeInput = inputIndex
+  inputs.value[inputIndex].$el.focus()
+  inputs.value[inputIndex].$el.select()
+}
+
+const focusNextInput = () => {
+  if (state.activeInput < props.characters - 1) {
+    focusInput(state.activeInput + 1)
+  }
+}
+
+const focusPreviousInput = () => {
+  if (state.activeInput > 0) {
+    focusInput(state.activeInput - 1)
+  }
+}
+
+// Handle cases of backspace, delete, left arrow, right arrow
+const onKeyDown = (index, event) => {
+  const BACKSPACE = 8
+  const LEFT_ARROW = 37
+  const RIGHT_ARROW = 39
+  const DELETE = 46
+
+  switch (event.keyCode) {
+    case BACKSPACE:
+      event.preventDefault()
+      onInput(index, "")
+      focusPreviousInput() // Stay in the same input
+      break
+    case DELETE:
+      event.preventDefault()
+      onInput(index, "")
+      focusPreviousInput() // Stay in the same input
+      break
+    case LEFT_ARROW:
+      event.preventDefault()
+      focusPreviousInput()
+      break
+    case RIGHT_ARROW:
+      event.preventDefault()
+      focusNextInput()
+      break
+    default:
+      break
+  }
+}
+</script>
--- src/views/sandbox/FormControlComponents.vue
+++ src/views/sandbox/FormControlComponents.vue
@@ -101,6 +101,14 @@
         :validation="v$.timeField"
       />
     </SandboxRow>
+    <SandboxRow title="VOtpField">
+      <VOtpField
+        v-model="form.otpField"
+        label="One Time Password Field"
+        :readonly="readonlyInputs"
+        :validation="v$.otpField"
+      />
+    </SandboxRow>
     <SandboxRow title="VCheckbox">
       <div class="flex items-center gap-2">
         <VCheckbox
@@ -163,7 +171,7 @@
 
 <script setup>
 import { computed, reactive, ref } from "vue"
-import { required } from "@vuelidate/validators"
+import { minLength, required } from "@vuelidate/validators"
 import useVuelidate from "@vuelidate/core"
 import PaintUnderline from "@/assets/paint-underline.svg"
 import SandboxRow from "@/views/sandbox/SandboxRow.vue"
@@ -182,6 +190,7 @@ const form = reactive({
   passwordField: "password",
   timeField: undefined,
   dateField: undefined,
+  otpField: "123456",
   textarea:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porttitor accumsan velit ac faucibus. Curabitur luctus, felis eu placerat suscipit, leo magna gravida lectus, non volutpat lacus nibh sed risus. Nulla eget sapien dignissim erat fringilla pretium. Proin aliquam accumsan dolor. Integer feugiat malesuada vehicula. Quisque lacinia vestibulum vehicula. Sed euismod aliquet tellus, nec rutrum velit ultrices at. Nulla ex ipsum, hendrerit sed mauris eget, faucibus malesuada est. Pellentesque vel nisi vel eros tempus feugiat.",
   selectedItem: undefined,
@@ -200,6 +209,7 @@ const rules = computed(() => ({
   passwordField: { required },
   timeField: { required },
   dateField: { required },
+  otpField: { required, minLength: minLength(6) },
   textarea: { required },
   selectedItem: { required },
   autocompleteItem: { required },
